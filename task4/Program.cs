3f3643aba470a266ab54f2b1a1395269fa60f0e0
Alexey <nikos_68@mail.ru>
2024-01-06 15:37:24 +0300

    add Task\task4

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 6;
+int b = 70;
+int c = 9;
+int d = 400;
+int e = 10;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+System.Console.WriteLine(max);

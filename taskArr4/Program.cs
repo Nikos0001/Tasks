cce6538c88f4055183f50f76dce3a980a4375326
Alexey <nikos_68@mail.ru>
2024-01-09 14:22:39 +0300

    Нахождение наибольшего числа массива

---
+++ taskArr4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] array = { 112, 4, 77, 3, 9 };
+int i = 0;
+int max = array[0];
+while (i<n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
